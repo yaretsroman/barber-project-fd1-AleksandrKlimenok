f72e4dea923ff230cb31af706344449bb1916918
raman martynau <yaretsroman@gmail.com>
2023-06-28 11:57:53 +0300

    added changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+import '@fortawesome/fontawesome-free/js/all';
 import './main.scss';
 import './components/typography/typography.scss';
+import './components/container.scss';
+import './components/header/header';
+import './components/top-navigation/top-navigation';
+import './components/header-banner/header-banner';
+import './components/button/button';
 
 console.log('Hello IT-academy student 🚀');
