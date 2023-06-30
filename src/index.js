1def5b90f648e5baf308a9fa98383996e1e56afb
raman martynau <yaretsroman@gmail.com>
2023-06-30 21:21:48 +0300

    added barbers banner

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import '@fortawesome/fontawesome-free/js/all';
+import 'swiper/css';
+
 import './main.scss';
 import './components/typography/typography.scss';
 import './components/container.scss';
@@ -8,5 +10,7 @@ import './components/header-banner/header-banner';
 import './components/button/button';
 import './components/benefits/benefits';
 import './components/about-us/about-us';
+import './components/testimonials/testimonials';
+import './components/barbers-banner/barbers-banner';
 
 console.log('Hello IT-academy student 🚀');
